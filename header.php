9bac51c122d451f1a4f51ff1eda2d703f5a008b1
Yee <yeekiwai@gmail.com>
2013-11-07 17:00:26 -0600

    @wip Working on dat admin panel

--- header.php
+++ header.php
@@ -10,12 +10,9 @@
 
 <div class="header">
     <span class="riesling"> 
-        <span style="font-size:120px; float:left; font-weight:300">Rynspyr</span>
-        <!-- <span style="font-size:64px; float:right; line-height: 165px;">Start About Misc</span> -->
-        <span style=" width: 200px;
-	display: block;
-    margin-left: auto;
-    margin-right: auto;"><img src="im/logo.png" /></span>
+        <span id="title">Rynspyr</span>
+        <nav>Start About Misc</nav>
+        <span class="logo_container"><img src="im/logo.png" /></span>
     </span>
 </div>
 
